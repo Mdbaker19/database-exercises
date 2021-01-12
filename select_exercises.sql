cf2ac655f22f422d716a09dd2873dab535080f95
Matthew <matthew.d.baker20@gmail.com>
2021-01-12 10:20:20 -0600

    updated table display structure with alias

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,19 +1,19 @@
 use codeup_test_db;
 
-select 'Pink floyd albums' as '';
-select name from albums where artist = 'Pink Floyd';
+select name AS 'Pink floyd albums';
+from albums where artist = 'Pink Floyd';
 
-select 'the year sgt. peppers''s lonely hearts club band was released' as '';
-select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+select release_date AS 'the year sgt. peppers''s lonely hearts club band was released';
+from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-select 'the genre of nevermind' as '';
-select genre from albums where name = 'Nevermind';
+select genre AS 'the genre of nevermind';
+from albums where name = 'Nevermind';
 
-select 'the albums released in the 90s' as '';
-select * from albums where release_date between 1990 and 1999;
+select name as 'the albums released in the 90s'
+from albums where release_date between 1990 and 1999;
 
-select 'the albums with less than 20 mil sales' as '';
-select * from albums where sales < 20;
+select NAME AS 'the albums with less than 20 mil sales'
+from albums where sales < 20;
 
-select 'the albums with genre of rock' as '';
-select * from albums where genre = 'rock';
+select name as 'the albums with genre of rock'
+from albums where genre = 'Rock';
