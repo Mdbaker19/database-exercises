2776977a93c329df7406a88c75ec8c397c16ec98
Matthew <matthew.d.baker20@gmail.com>
2021-01-12 14:53:58 -0600

    part one done

---
+++ where_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+
+DESCRIBE employees;
+
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+# SELECT hire_date FROM employees;
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT birth_date FROM employees;
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
