e1221b4bf1c7c3af81565d98f87b3e6486baf594
Matthew <matthew.d.baker20@gmail.com>
2021-01-13 10:08:48 -0600

    order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,57 @@
+USE employees;
+
+DESCRIBE employees;
+
+# SELECT first_name
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees;
+SELECT first_name, last_name, gender
+FROM employees
+WHERE gender = 'M' AND (
+            first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya')
+ORDER BY last_name DESC, first_name;
+
+
+
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%E' OR last_name LIKE 'E%';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
+
+# SELECT hire_date FROM employees;
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+# SELECT birth_date FROM employees;
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25' AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
